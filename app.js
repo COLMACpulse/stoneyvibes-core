6f7d55eb9f68bac1688fcbc84bf7a3feffb6520d
COLMACpulse <mccarthybpm@gmail.com>
2026-01-16 11:03:32 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 console.log("app.js loaded");
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("DOM ready");
+
+  const recBtn = document.getElementById("recBtn");
+  const stopBtn = document.getElementById("stopBtn");
+
+  if (recBtn) {
+    recBtn.onclick = () => console.log("REC clicked");
+  }
+
+  if (stopBtn) {
+    stopBtn.onclick = () => console.log("STOP clicked");
+  }
+});
